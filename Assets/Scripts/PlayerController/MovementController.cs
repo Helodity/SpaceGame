dfd3df07e14d6428a2d87ec9afc9b1a1add78b8c
Helodity <kbartlettofficial@gmail.com>
2025-12-16 17:05:23 -0500

    Refactor: Split movement into an abstract class

---
+++ Assets/Scripts/PlayerController/MovementController.cs
@@ -0,0 +1,183 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] protected float forwardMoveSpeed;
+    [SerializeField] protected float sideMoveSpeed;
+    [SerializeField] protected float reverseMoveSpeed;
+    [SerializeField] protected float maxMagnitude;
+    [SerializeField] protected bool useDampeners;
+
+    [Header("Rotation")]
+    [SerializeField] protected float maxTurnSpeed = 100;
+    [SerializeField] protected float maxTiltAngle = 30;
+    [SerializeField] protected float rollScale;
+    [SerializeField] protected float maxRollAngle;
+    [SerializeField] protected InterpolatedValue curTurnSpeed;
+    [SerializeField] protected InterpolatedValue xAngle;
+    [SerializeField] protected InterpolatedValue yAngle;
+
+    [Header("Visuals")]
+    [SerializeField] protected List<ParticleSystem> forwardThrusters = new List<ParticleSystem>();
+    [SerializeField] protected List<ParticleSystem> reverseThrusters = new List<ParticleSystem>();
+    [SerializeField] protected List<ParticleSystem> leftThrusters = new List<ParticleSystem>();
+    [SerializeField] protected List<ParticleSystem> rightThrusters = new List<ParticleSystem>();
+
+
+    protected Rigidbody rb;
+
+    #region Unity Events
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    protected void Update()
+    {
+        rotatePlayer();
+        applyAcceleration();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, getForwardVector());
+
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Gizmos.DrawWireSphere(mouseWorldPos, 1);
+        if(rb)
+        {
+            Gizmos.color = Color.cyan;
+            Vector2 accelVector = getAccelerationVector(useDampeners);
+            Gizmos.DrawRay(transform.position, (Vector2)transform.TransformVector(accelVector));
+        }
+    }
+    #endregion
+
+    #region Abstract Functions
+    protected abstract Vector3 getTargetPos();
+    protected abstract float getTargetAngle();
+    #endregion
+
+    protected void rotatePlayer()
+    {
+        float delta = Mathf.DeltaAngle(getCurrentAngle(), getTargetAngle());
+        curTurnSpeed.SetValue(MathUtils.BetterSign(delta) * maxTurnSpeed);
+        curTurnSpeed.Tick(Time.deltaTime);
+
+        float accel = (getAccelerationVector(useDampeners) / Time.deltaTime).x;
+        yAngle.SetValue(MathUtils.BetterSign(accel) * maxTiltAngle);
+        yAngle.Tick(Time.deltaTime);
+
+        float zAngle = Mathf.MoveTowardsAngle(getCurrentAngle(), getTargetAngle(), Mathf.Abs(curTurnSpeed.GetValue()) * Time.deltaTime);
+        float angleDiff = Mathf.Clamp(Mathf.DeltaAngle(getCurrentAngle(), getTargetAngle()), -maxRollAngle, maxRollAngle);
+        xAngle.SetValue(angleDiff * rollScale);
+        xAngle.Tick(Time.deltaTime);
+        transform.rotation = Quaternion.identity;
+        transform.Rotate(Vector3.right * xAngle.GetValue(), Space.World);
+        transform.Rotate(Vector3.up * yAngle.GetValue(), Space.World);
+        transform.Rotate(Vector3.forward * zAngle, Space.World);
+    }
+
+    protected Vector3 getForwardVector()
+    {
+        Vector3 vec = transform.right;
+        vec.z = 0;
+        return vec.normalized;
+    }
+
+    protected void applyAcceleration()
+    {
+        Vector2 moveVec = getAccelerationVector(useDampeners);
+
+        updateThrusterParticles(moveVec);
+
+        rb.velocity += transform.TransformVector(moveVec);
+
+        //Limit magnitude from acceleration
+        if(rb.velocity.magnitude > maxMagnitude)
+        {
+            rb.velocity = rb.velocity.normalized * maxMagnitude;
+        }
+    }
+
+    protected void updateThrusterParticles(Vector2 moveDir)
+    {
+        float x = MathUtils.BetterSign(moveDir.x);
+        switch(x)
+        {
+            case 1:
+                forwardThrusters.ForEach(x => x.Play());
+                reverseThrusters.ForEach(x => x.Stop());
+                break;
+            case -1:
+                forwardThrusters.ForEach(x => x.Stop());
+                reverseThrusters.ForEach(x => x.Play());
+                break;
+            default:
+                forwardThrusters.ForEach(x => x.Stop());
+                reverseThrusters.ForEach(x => x.Stop());
+                break;
+        }
+        float y = moveDir.y == 0 ? 0 : Mathf.Sign(moveDir.y);
+        switch(y)
+        {
+            case 1:
+                leftThrusters.ForEach(x => x.Play());
+                rightThrusters.ForEach(x => x.Stop());
+                break;
+            case -1:
+                leftThrusters.ForEach(x => x.Stop());
+                rightThrusters.ForEach(x => x.Play());
+                break;
+            default:
+                leftThrusters.ForEach(x => x.Stop());
+                rightThrusters.ForEach(x => x.Stop());
+                break;
+        }
+    }
+
+    protected float getCurrentAngle()
+    {
+        return Vector2.SignedAngle(Vector2.right, getForwardVector());
+    }
+
+    protected Vector2 getAccelerationVector(bool applyDampeners)
+    {
+        Quaternion angle = Quaternion.AngleAxis(-getCurrentAngle(), Vector3.forward);
+        Vector2 moveVec = angle * (getTargetPos() - transform.position);
+        
+        if(applyDampeners)
+        {
+            Vector2 targetSpeed = moveVec == Vector2.zero ? Vector2.zero : moveVec.normalized * maxMagnitude;
+            Vector2 curSpeed = angle * rb.velocity;
+            Vector2 thrustDir = (targetSpeed - curSpeed).normalized;
+            Vector2 maxAccel = getMaxMagnitude(thrustDir) * Time.deltaTime;
+
+            Vector2 speedOffset = targetSpeed - curSpeed;
+            if(targetSpeed == Vector2.zero && speedOffset.magnitude < maxAccel.magnitude)
+            {
+                return speedOffset;
+            }
+
+            return maxAccel;
+        } else
+        {
+            moveVec = moveVec.normalized;
+            moveVec.x *= moveVec.x < 0 ? reverseMoveSpeed : forwardMoveSpeed;
+            moveVec.y *= sideMoveSpeed;
+            return moveVec * Time.deltaTime;
+        }
+    }
+
+
+    protected Vector2 getMaxMagnitude(Vector2 dir)
+    {
+        if(dir == Vector2.zero) return Vector2.zero;
+        dir = dir.normalized;
+        float angle = Mathf.Deg2Rad * Vector2.SignedAngle(Vector2.right, dir);
+        float maxXspeed = Mathf.Cos(angle) < 0 ? reverseMoveSpeed : forwardMoveSpeed;
+        return new Vector2(maxXspeed * Mathf.Cos(angle), sideMoveSpeed * Mathf.Sin(angle));
+    }
+}
--- Assets/Scripts/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -1,44 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MovementController
 {
-    [Header("Movement")]
-    [SerializeField] float forwardMoveSpeed;
-    [SerializeField] float sideMoveSpeed;
-    [SerializeField] float reverseMoveSpeed;
-    [SerializeField] float maxMagnitude;
-    [SerializeField] bool useDampeners;
-    [SerializeField] bool mouseMovement;
-
-    [Header("Rotation")]
-    [SerializeField] float maxTurnSpeed = 100;
-    [SerializeField] float maxTiltAngle = 30;
-    [SerializeField] float rollScale;
-    [SerializeField] float maxRollAngle;
-    [SerializeField] InterpolatedValue curTurnSpeed;
-    [SerializeField] InterpolatedValue xAngle;
-    [SerializeField] InterpolatedValue yAngle;
-
-    [Header("Visuals")]
-    [SerializeField] List<ParticleSystem> forwardThrusters = new List<ParticleSystem>();
-    [SerializeField] List<ParticleSystem> reverseThrusters = new List<ParticleSystem>();
-    [SerializeField] List<ParticleSystem> leftThrusters = new List<ParticleSystem>();
-    [SerializeField] List<ParticleSystem> rightThrusters = new List<ParticleSystem>();
-
-
-    Rigidbody rb;
 
     #region Unity Events
-    void Start()
+    protected new void Update()
     {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        rotatePlayer();
-        applyAcceleration();
+        base.Update();
         if(Input.GetKeyDown(KeyCode.LeftControl))
         {
             useDampeners = !useDampeners;
@@ -51,172 +20,30 @@ public class PlayerMovement : MonoBehaviour
                 Camera.main.transform.position.z
             );
     }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(transform.position, getForwardVector());
-
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Gizmos.DrawWireSphere(mouseWorldPos, 1);
-        if(rb)
-        {
-            Gizmos.color = Color.cyan;
-            Vector2 accelVector = getAccelerationVector(useDampeners);
-            Gizmos.DrawRay(transform.position, (Vector2)transform.TransformVector(accelVector));
-        }
-    }
     #endregion
 
-    void rotatePlayer()
-    {
-        float delta = Mathf.DeltaAngle(getCurrentAngle(), getTargetAngle());
-        curTurnSpeed.SetValue(MathUtils.BetterSign(delta) * maxTurnSpeed);
-        curTurnSpeed.Tick(Time.deltaTime);
-
-        float accel = (getAccelerationVector(useDampeners) / Time.deltaTime).x;
-        yAngle.SetValue(MathUtils.BetterSign(accel) * maxTiltAngle);
-        yAngle.Tick(Time.deltaTime);
-
-        float zAngle = Mathf.MoveTowardsAngle(getCurrentAngle(), getTargetAngle(), Mathf.Abs(curTurnSpeed.GetValue()) * Time.deltaTime);
-        float angleDiff = Mathf.Clamp(Mathf.DeltaAngle(getCurrentAngle(), getTargetAngle()), -maxRollAngle, maxRollAngle);
-        xAngle.SetValue(angleDiff * rollScale);
-        xAngle.Tick(Time.deltaTime);
-        transform.rotation = Quaternion.identity;
-        transform.Rotate(Vector3.right * xAngle.GetValue(), Space.World);
-        transform.Rotate(Vector3.up * yAngle.GetValue(), Space.World);
-        transform.Rotate(Vector3.forward * zAngle, Space.World);
-    }
-
-
-    Vector3 getForwardVector()
-    {
-        Vector3 vec = transform.right;
-        vec.z = 0;
-        return vec.normalized;
-    }
-
-    void applyAcceleration()
-    {
-        Vector2 moveVec = getAccelerationVector(useDampeners);
-
-        updateThrusterParticles(moveVec);
-
-        rb.velocity += transform.TransformVector(moveVec);
-
-        //Limit magnitude from acceleration
-        if(rb.velocity.magnitude > maxMagnitude)
-        {
-            rb.velocity = rb.velocity.normalized * maxMagnitude;
-        }
-    }
-
-    void updateThrusterParticles(Vector2 moveDir)
-    {
-        float x = MathUtils.BetterSign(moveDir.x);
-        switch(x)
-        {
-            case 1:
-                forwardThrusters.ForEach(x => x.Play());
-                reverseThrusters.ForEach(x => x.Stop());
-                break;
-            case -1:
-                forwardThrusters.ForEach(x => x.Stop());
-                reverseThrusters.ForEach(x => x.Play());
-                break;
-            default:
-                forwardThrusters.ForEach(x => x.Stop());
-                reverseThrusters.ForEach(x => x.Stop());
-                break;
-        }
-        float y = moveDir.y == 0 ? 0 : Mathf.Sign(moveDir.y);
-        switch(y)
-        {
-            case 1:
-                leftThrusters.ForEach(x => x.Play());
-                rightThrusters.ForEach(x => x.Stop());
-                break;
-            case -1:
-                leftThrusters.ForEach(x => x.Stop());
-                rightThrusters.ForEach(x => x.Play());
-                break;
-            default:
-                leftThrusters.ForEach(x => x.Stop());
-                rightThrusters.ForEach(x => x.Stop());
-                break;
-        }
-    }
-
-    float getTargetAngle()
-    {
-        return Vector2.SignedAngle(Vector2.right, getMouseWorldPos() - transform.position);
-    }
-
-    float getCurrentAngle()
-    {
-        return Vector2.SignedAngle(Vector2.right, getForwardVector());
-    }
-
-    Vector3 getMouseWorldPos()
+    Vector3 getMousePos()
     {
         return Camera.main.ScreenToWorldPoint(
-            new Vector3(
-                Input.mousePosition.x, 
-                Input.mousePosition.y, 
-                transform.position.z - Camera.main.transform.position.z
-            )
-        );
+                new Vector3(
+                    Input.mousePosition.x, 
+                    Input.mousePosition.y, 
+                    transform.position.z - Camera.main.transform.position.z
+                )
+            );
     }
 
-
-    Vector2 getAccelerationVector(bool applyDampeners)
+    protected override Vector3 getTargetPos()
     {
-        Vector2 moveVec = Vector2.zero;
-        Quaternion angle = Quaternion.AngleAxis(-getCurrentAngle(), Vector3.forward);
-        if(mouseMovement)
+        if (Input.GetMouseButton(1))
         {
-            if(Input.GetMouseButton(1))
-            {
-                Vector3 mouseWorldPos = getMouseWorldPos();
-                moveVec = angle * (mouseWorldPos - transform.position);
-                if(!applyDampeners)
-                {
-                    moveVec = moveVec.normalized;
-                }
-            }
-        } else
-        {
-            moveVec = new Vector2(Input.GetAxisRaw("Vertical"), -Input.GetAxisRaw("Horizontal"));
+            return getMousePos();
         }
-        
-        if(applyDampeners)
-        {
-            Vector2 targetSpeed = moveVec == Vector2.zero ? Vector2.zero : moveVec.normalized * maxMagnitude;
-            Vector2 curSpeed = angle * rb.velocity;
-            Vector2 thrustDir = (targetSpeed - curSpeed).normalized;
-            Vector2 maxAccel = getMaxMagnitude(thrustDir) * Time.deltaTime;
-
-            Vector2 speedOffset = targetSpeed - curSpeed;
-            if(targetSpeed == Vector2.zero && speedOffset.magnitude < maxAccel.magnitude)
-            {
-                return speedOffset;
-            }
-
-            return maxAccel;
-        } else
-        {
-            moveVec.x *= moveVec.x < 0 ? reverseMoveSpeed : forwardMoveSpeed;
-            moveVec.y *= sideMoveSpeed;
-            return moveVec * Time.deltaTime;
-        }
+        return transform.position;
     }
 
-
-    Vector2 getMaxMagnitude(Vector2 dir)
+    protected override float getTargetAngle()
     {
-        if(dir == Vector2.zero) return Vector2.zero;
-        dir = dir.normalized;
-        float angle = Mathf.Deg2Rad * Vector2.SignedAngle(Vector2.right, dir);
-        float maxXspeed = Mathf.Cos(angle) < 0 ? reverseMoveSpeed : forwardMoveSpeed;
-        return new Vector2(maxXspeed * Mathf.Cos(angle), sideMoveSpeed * Mathf.Sin(angle));
+        return Vector2.SignedAngle(Vector2.right, getMousePos() - transform.position);
     }
 }
