37f0124b1b00363654b3224c7990a96fc5d445b2
Helodity <kbartlettofficial@gmail.com>
2025-12-18 17:00:18 -0500

    Half decent leading algorithm

--- Assets/Scripts/Ships/TurnToPlayerMovement.cs
+++ Assets/Scripts/Ships/TurnToPlayerMovement.cs
@@ -1,19 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnToPlayerMovement : MovementController
 {
     [SerializeField] Rigidbody target;
+    [SerializeField] float bulletSpeed;
 
+    Rigidbody self;
+
+    void Awake()
+    {
+        self = GetComponent<Rigidbody>();
+    }
     Vector3 leadTarget()
     {
-        return target.position + target.velocity;
+        int iterations = 10;
+        Vector3 location;
+        float dist = (target.position - transform.position).magnitude;
+        for(int i = 0; i < iterations; i++)
+        {
+            float dur = dist / bulletSpeed;
+            location = getTargetAtTime(dur);
+            dist = (location - transform.position).magnitude;
+        }
+
+        return getTargetAtTime(dist / bulletSpeed);
+    }
+
+    Vector3 getTargetAtTime(float duration)
+    {
+        self = GetComponent<Rigidbody>();
+        return target.position + ((target.velocity - self.velocity) * duration);
     }
 
     protected override float getTargetAngle()
     {
-        return Vector2.SignedAngle(Vector2.right, target.position - transform.position);
+        return Vector2.SignedAngle(Vector2.right, leadTarget() - transform.position);
     }
 
     protected override Vector3 getTargetPos()
