a95cbbdefbd896b971f8680843d031389aca8459
Helodity <kbartlettofficial@gmail.com>
2025-12-21 20:50:12 -0500

    Un loopify bullet leading

--- Assets/Scripts/Ships/TurnToPlayerMovement.cs
+++ Assets/Scripts/Ships/TurnToPlayerMovement.cs
@@ -7,28 +7,30 @@ public class TurnToPlayerMovement : MovementController
 
     Rigidbody self;
 
-    void Awake()
-    {
-        self = GetComponent<Rigidbody>();
-    }
     Vector3 leadTarget()
     {
-        int iterations = 10;
-        Vector3 location;
-        float dist = (target.position - transform.position).magnitude;
-        for(int i = 0; i < iterations; i++)
-        {
-            float dur = dist / bulletSpeed;
-            location = getTargetAtTime(dur);
-            dist = (location - transform.position).magnitude;
-        }
+        // Math from 
+        // https://medium.com/andys-coding-blog/ai-projectile-intercept-formula-for-gaming-without-trigonometry-37b70ef5718b
+        if(!self) self = GetComponent<Rigidbody>();
 
-        return getTargetAtTime(dist / bulletSpeed);
+        Vector3 pA = target.transform.position - transform.position;
+        Vector3 vA = target.velocity - self.velocity;
+
+        float coeffA = Vector3.Dot(vA, vA) - bulletSpeed * bulletSpeed;
+        float coeffB = 2 * Vector3.Dot(pA, vA);
+        float coeffC = Vector3.Dot(pA, pA);
+
+        float t1 = (-coeffB + Mathf.Sqrt((coeffB * coeffB) - 4 * coeffA * coeffC)) / (2 * coeffA);
+        float t2 = (-coeffB - Mathf.Sqrt((coeffB * coeffB) - 4 * coeffA * coeffC)) / (2 * coeffA);
+
+        if(!float.IsNaN(t1) && t1 >= 0) return getTargetAtTime(t1);
+        if(!float.IsNaN(t2) && t2 >= 0) return getTargetAtTime(t2);
+
+        return getTargetAtTime(0);
     }
 
     Vector3 getTargetAtTime(float duration)
     {
-        self = GetComponent<Rigidbody>();
         return target.position + ((target.velocity - self.velocity) * duration);
     }
 
@@ -39,6 +41,6 @@ public class TurnToPlayerMovement : MovementController
 
     protected override Vector3 getTargetPos()
     {
-        return target.position;
+        return target.position + target.transform.up * 5;
     }
 }
